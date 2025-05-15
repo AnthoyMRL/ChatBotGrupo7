ceb052552d11e9eaa2c815eeb1d7e276d388eecd
AnthonyMRL <am4866692@gmail.com>
2025-05-15 07:44:37 -0500

    Fue agregado el Api Key de Gemini

--- ChatBotGrupo7/Repository/GeminiRepository.cs
+++ ChatBotGrupo7/Repository/GeminiRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace ChatBotGrupo7.Repository
+﻿using ChatBotGrupo7.Interface;
+
+namespace ChatBotGrupo7.Repository
 {
-    public class GeminiRepository
+    public class GeminiRepository : IChatBotService
     {
+        private HttpClient _httpClient;
+        private string geminiApiKey = "AIzaSyATksFNyjGqbmkp3WXQTmBEvJYe_gKpLrg"
+
+        public GeminiRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public Task<string> GetChatbotResponse(string prompt)
+        {
+            string url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +geminiApiKey;
+        }
+
+        public Task<bool> SaveResponseInDatabase(string response)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
 }
