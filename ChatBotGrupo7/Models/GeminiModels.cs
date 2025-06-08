dd36b10fdd73817c8c026fb9b93e1529af77d653
AnthonyMRL <anthony.morales.velez@udla.edu.ec>
2025-06-08 12:59:51 -0500

    Cambio y agregar un modelo

--- ChatBotGrupo7/Models/GeminiModels.cs
+++ ChatBotGrupo7/Models/GeminiModels.cs
@@ -4,15 +4,20 @@
     {
         public List<GeminiContent> contents { get; set; }
     }
-
     public class GeminiContent
     {
         public List<GeminiPart> parts { get; set; }
     }
-
     public class GeminiPart
     {
         public string text { get; set; }
     }
-
+    public class GeminiResponse
+    {
+        public List<GeminiCandidate> candidates { get; set; }
+    }
+    public class GeminiCandidate
+    {
+        public GeminiContent content { get; set; }
+    }
 }
